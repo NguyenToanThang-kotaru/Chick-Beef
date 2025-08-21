af2efbfcc6c9b4c822f73b08dbcef228cf79892e
ALDEN <trungmai12121970@gmail.com>
2025-08-21 20:48:37 +0700

    test

--- Client/src/pages/admin/Accounts/Accounts.jsx
+++ Client/src/pages/admin/Accounts/Accounts.jsx
@@ -5,6 +5,8 @@ export default function Accounts() {
     <div>
         <h1 className="text-2xl">This is Accounts Page</h1>
         <h2>hello</h2>
+        <h1>Hello i am Mai trung </h1>
+        <h1>This is my page</h1>
     </div>
   );
 }
