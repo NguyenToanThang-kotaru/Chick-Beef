e8766786e078f686074e0aebbb9b672ff0953a3d
NguyenToanThang-kotaru <NguenToanThang15122005@gmail.com>
2025-08-22 05:26:53 +0700

    xong layout thogn ke invoice

---
+++ Client/src/Components/searchBar.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { Search } from "lucide-react"; // icon từ lucide-react
+import clearSearch from "../assets/Icon/clearSearch.png";
+
+export default function SearchBar({ placeholder = "Tìm kiếm...", onSearch }) {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = () => {
+    if (onSearch) onSearch(query.trim());
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div className="flex items-center w-full max-w-md bg-white rounded-4xl shadow-md px-3 py-2">
+      {/* Icon */}
+      <Search className="w-5 h-5 text-gray-400" />
+
+      {/* Input */}
+      <input
+        type="text"
+        value={query}
+        placeholder={placeholder}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={handleKeyDown}
+        className="flex-1 px-3 py-1 outline-none bg-transparent text-gray-700"
+      />
+
+      {/* Button search */}
+      <button onClick={handleSearch}>
+        <img src={clearSearch} alt="" />
+      </button>
+    </div>
+  );
+}
---
+++ Client/src/pages/admin/Dashboard/Statictics.jsx
@@ -0,0 +1,168 @@
+import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+import "../../../../src/index.css";
+
+export default function statictics() {
+  const data = [
+    { name: "Mang về", value: 60 },
+    { name: "Tại quán", value: 40 },
+  ];
+  const COLORS = ["#FACC15", "#22C55E"]; // vàng và xanh lá
+
+  return (
+    <div className="w-full flex h-full gap-5 ">
+      {/* Trạng thái tồn kho */}
+      <div className="bg-white p-4 h-full w-1/5 rounded-2xl shadow-md">
+        <h2 className="font-bold mb-2 text-mainBlue">Trạng Thái Tồn Kho</h2>
+
+        <div className="space-y-2 h-11/12 overflow-y-auto scroll-fade">
+          <div className="bg-mainRed border-4 border-[#8C0004] justify-between items-center flex text-white p-2 rounded-3xl font-bold">
+            <div>
+              <div>Nguyên liệu 1</div>
+              <div>Còn 2 (Đơn vị tính)</div>
+            </div>
+            <div className="text-center ">Sắp hết</div>
+          </div>
+          <div className="bg-mainRed border-4 border-[#8C0004] justify-between items-center flex text-white p-2 rounded-3xl font-bold">
+            <div>
+              <div>Nguyên liệu 2</div>
+              <div>Còn 2 (Đơn vị tính)</div>
+            </div>
+            <div className="text-center ">Sắp hết</div>
+          </div>
+          <div className="bg-mainRed border-4 border-[#8C0004] justify-between items-center flex text-white p-2 rounded-3xl font-bold">
+            <div>
+              <div>Nguyên liệu 3</div>
+              <div>Còn 2 (Đơn vị tính)</div>
+            </div>
+            <div className="text-center ">Sắp hết</div>
+          </div>
+          <div className="bg-mainRed border-4 border-[#8C0004] justify-between items-center flex text-white p-2 rounded-3xl font-bold">
+            <div>
+              <div>Nguyên liệu 4</div>
+              <div>Còn 2 (Đơn vị tính)</div>
+            </div>
+            <div className="text-center ">Sắp hết</div>
+          </div>
+          <div className="bg-mainRed border-4 border-[#8C0004] justify-between items-center flex text-white p-2 rounded-3xl font-bold">
+            <div>
+              <div>Nguyên liệu 5</div>
+              <div>Còn 2 (Đơn vị tính)</div>
+            </div>
+            <div className="text-center ">Sắp hết</div>
+          </div>
+          <div className="bg-mainRed border-4 border-[#8C0004] justify-between items-center flex text-white p-2 rounded-3xl font-bold">
+            <div>
+              <div>Nguyên liệu 6</div>
+              <div>Còn 2 (Đơn vị tính)</div>
+            </div>
+            <div className="text-center ">Sắp hết</div>
+          </div>
+          {/* <div className="bg-mainRed border-4 border-[#8C0004] justify-between items-center flex text-white p-2 rounded-3xl font-bold">
+            <div>
+              <div>Nguyên liệu 7</div>
+              <div>Còn 2 (Đơn vị tính)</div>
+            </div>
+            <div className="text-center ">Sắp hết</div>
+          </div>
+          <div className="bg-mainRed border-4 border-[#8C0004] justify-between items-center flex text-white p-2 rounded-3xl font-bold">
+            <div>
+              <div>Nguyên liệu 8</div>
+              <div>Còn 2 (Đơn vị tính)</div>
+            </div>
+            <div className="text-center ">Sắp hết</div>
+          </div> */}
+        </div>
+      </div>
+
+      <div className="h-full w-full flex gap-5 flex-col">
+        <div className="flex h-2/3 gap-5 w-full">
+          {/* Doanh thu theo tháng */}
+          <div className="bg-white p-4 rounded-2xl shadow-md w-3/4">
+            <h2 className="font-bold mb-2 border-b-mainBlue border-b-4 text-mainBlue text-2xl">
+              Doanh Thu Theo Tháng
+            </h2>
+            <div className="flex flex-col justify-between h-10/12">
+              {/* Danh sách ngày + giá */}
+              <ul className="space-y-1 overflow-y-auto">
+                <li className="flex font-bold text-2xl text-mainBlue justify-between">
+                  <span>17/8</span>
+                  <span>18.000.000 VND</span>
+                </li>
+                <li className="flex font-bold text-2xl text-mainBlue justify-between">
+                  <span>16/8</span>
+                  <span>18.000.000 VND</span>
+                </li>
+                <li className="flex font-bold text-2xl text-mainBlue justify-between">
+                  <span>15/8</span>
+                  <span>18.000.000 VND</span>
+                </li>
+                <li className="flex font-bold text-2xl text-mainBlue justify-between">
+                  <span>15/8</span>
+                  <span>18.000.000 VND</span>
+                </li>                <li className="flex font-bold text-2xl text-mainBlue justify-between">
+                  <span>15/8</span>
+                  <span>18.000.000 VND</span>
+                </li>
+              </ul>
+
+              {/* Tổng */}
+              <p className="mt-2 font-semibold text-left text-2xl text-mainBlue">
+                Tổng: 18.000.000 VND
+              </p>
+            </div>
+          </div>
+
+          {/* Biểu đồ tỉ lệ */}
+          <div className="bg-white p-4 rounded-2xl w-1/4 shadow-md">
+            <h2 className="font-bold mb-2">Tỉ Lệ</h2>
+            <div className="flex justify-center">
+              {/* Bạn có thể thay bằng chart từ recharts */}
+              <PieChart width={200} height={200}>
+                <Pie
+                  data={data}
+                  cx="50%"
+                  cy="50%"
+                  innerRadius={50}
+                  outerRadius={80}
+                  dataKey="value"
+                >
+                  {data.map((entry, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index % COLORS.length]}
+                    />
+                  ))}
+                </Pie>
+                <Tooltip />
+              </PieChart>
+            </div>
+            <div className="mt-2 flex-col text-center text-sm">
+              <div className="flex items-center justify-center gap-1">
+                <div className="w-3 h-3 bg-yellow-300 inline-block"></div> Mang
+                về
+              </div>
+              <div className="flex items-center justify-center gap-1">
+                <div className="w-3 h-3 bg-green-500 inline-block"></div> Tại
+                quán
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Top 5 món bán chạy */}
+        <div className="bg-white h-1/3 p-4 rounded-2xl shadow-md ">
+          <h2 className="font-bold mb-2 text-2xl text-mainBlue w-full border-b-4 border-b-mainBlue">
+            Top 5 Món Bán Chạy
+          </h2>
+          <ul className="space-y-1">
+            <li>Món 1: 55</li>
+            <li>Món 2: 40</li>
+            <li>Món 3: 32</li>
+            <li>Món 4: 19</li>
+            <li>Món 5: 9</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
