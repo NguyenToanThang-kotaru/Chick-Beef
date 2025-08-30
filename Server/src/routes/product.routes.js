4d01c32a50debc9ba39c846cf59d7de1929cbbc6
phattin <tin220905@gmail.com>
2025-08-30 20:14:36 +0700

    them getAllProducts

--- Server/src/routes/product.routes.js
+++ Server/src/routes/product.routes.js
@@ -7,22 +7,25 @@ const e = require('express');
 // Đếm sản phẩm bán chạy nhất
 router.get('/count', authenticateToken.authenticateToken, productController.countProduct);
 
-// GET /api/product/nextid
+// GET /api/product/nextid (lấy mã tiếp theo)
 router.get('/nextid', authenticateToken.authenticateToken, productController.getNextProductId);
 
-// GET /api/product/search?keyword=abc
+// GET /api/product/search?keyword=abc (tìm kiếm)
 router.get('/search', authenticateToken.authenticateToken, productController.searchProduct);
 
-// PUT /api/product/update/:id
+// GET /api/product (lấy tất cả)
+router.get('/', authenticateToken.authenticateToken, productController.getAllProducts);
+
+// PUT /api/product/update/:id (cập nhật)
 router.put('/update/:id', authenticateToken.authenticateToken, productController.updateProduct);
 
-// PUT /api/product/delete/:id
+// PUT /api/product/delete/:id (xóa)
 router.put('/delete/:id', authenticateToken.authenticateToken, productController.deleteProduct);
 
-// GET /api/product/:id
+// GET /api/product/:id (lấy theo id)
 router.get('/:id', authenticateToken.authenticateToken, productController.getProductById);
 
-// POST /api/product
+// POST /api/product (thêm mới)
 router.post('/', authenticateToken.authenticateToken, productController.addProduct);
 
 exports = module.exports = router;
