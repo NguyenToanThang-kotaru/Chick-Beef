29e20391cf832ee0abe23875185e60358b5da671
ALDEN <trungmai12121970@gmail.com>
2025-09-03 15:29:56 +0700

    merge main lúc 3:29pm 3/9

--- Client/src/pages/admin/Accounts/Accounts.jsx
+++ Client/src/pages/admin/Accounts/Accounts.jsx
@@ -4,6 +4,8 @@ import SearchBar from "@/Components/searchBar";
 import Table from "../../../Components/table_cpn";
 import eye from "../../../assets/Icon/Eye.png";
 import pd from "../../../assets/Icon/product.png";
+import Delete  from "../../../assets/Icon/delete.png";
+import Edit from "../../../assets/Icon/Edit.png";
 import AddAccount from "./AddAcount";
 import EditAccount from "./EditAccount";
 import WatchAccount from "./WatchAccount"
@@ -34,10 +36,10 @@ export default function Accounts() {
       <div className="bg-[#FFF8F0] p-4 rounded-2xl shadow-md">
           <Table
             data={[
-              { ID: <img src={pd} alt="eye" className="w-25 h-25 cursor-pointer" />, Username: 123456789, Password: "Newwwwwwwwwwww", State: "Gôddddddddd", Role: "JohnDoe", Position: "123-456-7890", watch: <img src={eye} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowWatchAccount(true)}/>,edit: <img src={eye} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowEditAccount(true)} />},
-              { ID: <img src={pd} alt="eye" className="w-25 h-25 cursor-pointer" />, Username: 123456789, Password: "Newwwwwwwwwwww", State: "Gôddddddddd", Role: "JohnDoe", Position: "123-456-7890", watch: <img src={eye} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowWatchAccount(true)}/>,edit: <img src={eye} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowEditAccount(true)} />},
-              { ID: <img src={pd} alt="eye" className="w-25 h-25 cursor-pointer" />, Username: 123456789, Password: "Newwwwwwwwwwww", State: "Gôddddddddd", Role: "JohnDoe", Position: "123-456-7890", watch: <img src={eye} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowWatchAccount(true)}/>,edit: <img src={eye} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowEditAccount(true)} />},
-              { ID: <img src={pd} alt="eye" className="w-25 h-25 cursor-pointer" />, Username: 123456789, Password: "Newwwwwwwwwwww", State: "Gôddddddddd", Role: "JohnDoe", Position: "123-456-7890", watch: <img src={eye} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowWatchAccount(true)}/>,edit: <img src={eye} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowEditAccount(true)} />},
+              { ID: <img src={pd} alt="eye" className="w-25 h-25 cursor-pointer" />, Username: 123456789, Password: "Newwwwwwwwwwww", State: "Gôddddddddd", Role: "JohnDoe", Position: "123-456-7890", watch: <img src={eye} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowWatchAccount(true)}/>,edit: <img src={Edit} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowEditAccount(true)} />,Delete: <img src={Delete} className="w-6 h-6 cursor-pointer"/>},
+              { ID: <img src={pd} alt="eye" className="w-25 h-25 cursor-pointer" />, Username: 123456789, Password: "Newwwwwwwwwwww", State: "Gôddddddddd", Role: "JohnDoe", Position: "123-456-7890", watch: <img src={eye} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowWatchAccount(true)}/>,edit: <img src={Edit} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowEditAccount(true)} />,Delete: <img src={Delete} className="w-6 h-6 cursor-pointer"/>},
+              { ID: <img src={pd} alt="eye" className="w-25 h-25 cursor-pointer" />, Username: 123456789, Password: "Newwwwwwwwwwww", State: "Gôddddddddd", Role: "JohnDoe", Position: "123-456-7890", watch: <img src={eye} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowWatchAccount(true)}/>,edit: <img src={Edit} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowEditAccount(true)} />,Delete: <img src={Delete} className="w-6 h-6 cursor-pointer"/>},
+              { ID: <img src={pd} alt="eye" className="w-25 h-25 cursor-pointer" />, Username: 123456789, Password: "Newwwwwwwwwwww", State: "Gôddddddddd", Role: "JohnDoe", Position: "123-456-7890", watch: <img src={eye} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowWatchAccount(true)}/>,edit: <img src={Edit} alt="eye" className="w-6 h-6 cursor-pointer" onClick={()=>setShowEditAccount(true)} />,Delete: <img src={Delete} className="w-6 h-6 cursor-pointer"/>},
           ]}
           />
       </div>
